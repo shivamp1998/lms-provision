65ea82a3632e5ab1e8dba075fb2872ef3f150c71
shivamp1998 <shivampandey98@gmail.com>
2024-05-27 11:57:44 +0530

    fixes

--- src/controllers/provision.controller.ts
+++ src/controllers/provision.controller.ts
@@ -1,25 +1,32 @@
-import { Request, Response, NextFunction} from 'express';
-import Provision from '../factory/provision.factory';
-import  { deprovisionQueue } from '../services/bullmq.service'
+import { Request, Response, NextFunction } from "express";
+import Provision from "../factory/provision.factory";
+import { deprovisionQueue } from "../services/bullmq.service";
 
-
-const provisionController = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const bucketName = req.body["bucketName"];
-      const instance = new Provision(bucketName);
-      const provisioned = await instance.initializeProvision();
-      deprovisionQueue.add('deprovisionQueue', {
+const provisionController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const bucketName = req.body["bucketName"];
+    const instance = new Provision(bucketName);
+    const provisioned = await instance.initializeProvision();
+    deprovisionQueue.add(
+      "deprovisionQueue",
+      {
         bucketName: bucketName,
-      }, {
-        delay:  5 * 60 * 1000,
-      })
-      return res.send(({bucketName, provisioned}))  
-    }catch(err) {
-        console.log(err);
-       next(err)
-    }
-}
+      },
+      {
+        delay: 30 * 60 * 1000,
+      }
+    );
+    return res.send({ bucketName, provisioned });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
 
 export default {
-    provisionController
-}
+  provisionController,
+};
--- src/factory/provision.factory.ts
+++ src/factory/provision.factory.ts
@@ -1,9 +1,9 @@
-import util from 'util'
-import {execFileSync} from 'child_process'
-import path from 'path';
+import util from "util";
+import { execFileSync } from "child_process";
+import path from "path";
 import AWS, { S3 } from "aws-sdk";
 import axios from "axios";
-import { stderr, stdout } from 'process';
+import { stderr, stdout } from "process";
 AWS.config.update({
   region: "ap-south-1",
   accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -28,39 +28,47 @@ class Provision {
         .join("\n");
       const params = {
         Bucket: "lmsfrontend",
-        Key: `${this.bucketName}.tfvars`,
-        Body: tfVarsContent,
-        ContentType: "text/plain",
       };
 
+      const uploadFileToBucket = (bucket: string) => {
+        const params = {
+          Bucket: bucket,
+          Key: `${this.bucketName}.tfvars`,
+          Body: tfVarsContent,
+          ContentType: "text/plain",
+        };
+        return s3.upload(params).promise();
+      };
+
+      const promises = ["lmsfrontend", "lmsbackend"].map((bucket) =>
+        uploadFileToBucket(bucket)
+      );
+      const results = await Promise.all(promises);
       let response;
-      s3.upload(params, async (err: Error, data: S3.ManagedUpload.SendData) => {
-        if (err) {
-        } else {
-          response = await axios.post(
-            `https://gitlab.com/api/v4/projects/57900971/trigger/pipeline?token=${process.env.PERSONAL_ACCESS_TOKEN}&ref=main`,
-            { variables: variables }
-          );
-        }
-      });
-      return response;
+      if (results.length == 2) {
+        response = await axios.post(
+          `https://gitlab.com/api/v4/projects/57900971/trigger/pipeline?token=${process.env.PERSONAL_ACCESS_TOKEN}&ref=main`,
+          { variables: variables }
+        );
+        console.log(results, "what is response", response);
+      }
+      return { success: true };
     } catch (err) {
       console.log(err);
     }
   }
   async initializeDeprovision() {
     try {
-      const scriptPath = path.join(process.cwd(), 'deprovision.sh');
-      const fs = require('fs');
+      const scriptPath = path.join(process.cwd(), "deprovision.sh");
+      const fs = require("fs");
       if (!fs.existsSync(scriptPath)) {
         throw new Error(`File not found: ${scriptPath}`);
       }
 
-      execFileSync('bash', [scriptPath, this.bucketName], {stdio: 'inherit'});
-    }catch(err) {
-      console.log(err)
+      execFileSync("bash", [scriptPath, this.bucketName], { stdio: "inherit" });
+    } catch (err) {
+      console.log(err);
     }
-    
   }
 }
 
